64f0f88627ab92ce6486ea9ee4bd520289c33c0c
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:11 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,30 +1,30 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'subscriptions'
-    MODULE_NAME = _('Subscriptions & Memberships')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'repeat-outline'
-    MODULE_DESCRIPTION = _('Recurring billing, plans and membership management')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'finance'
+MODULE_ID = 'subscriptions'
+MODULE_NAME = _('Subscriptions & Memberships')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'repeat-outline'
+MODULE_DESCRIPTION = _('Recurring billing, plans and membership management')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'finance'
 
-    MENU = {
-        'label': _('Subscriptions & Memberships'),
-        'icon': 'repeat-outline',
-        'order': 46,
-    }
+MENU = {
+    'label': _('Subscriptions & Memberships'),
+    'icon': 'repeat-outline',
+    'order': 46,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Plans'), 'icon': 'layers-outline', 'id': 'plans'},
 {'label': _('Subscriptions'), 'icon': 'repeat-outline', 'id': 'subscriptions'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'subscriptions.view_subscription',
+PERMISSIONS = [
+    'subscriptions.view_subscription',
 'subscriptions.add_subscription',
 'subscriptions.change_subscription',
 'subscriptions.delete_subscription',
@@ -32,4 +32,4 @@
 'subscriptions.add_plan',
 'subscriptions.change_plan',
 'subscriptions.manage_settings',
-    ]
+]
